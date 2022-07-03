b1b72162664828a159bb4010f08e8d15567dcbb9
Yoon Lee <44100763+yoonix1@users.noreply.github.com>
2022-07-03 15:35:14 -0700

    This should work

--- process.py
+++ process.py
@@ -1,5 +1,6 @@
 import pathlib
 import os
+import subprocess
 
 # this is a help program to convert all files under "to_convert" to convert.
 # if the directory has the subdirectories those will be converte
@@ -14,6 +15,16 @@ tempfile = 'processed.avi'
 # iterate over files in
 # that directory
 
+def doFile(filename):
+    p = subprocess.Popen(['ffprobe', filename],stderr=subprocess.PIPE, stdout=subprocess.PIPE)
+    p.wait()
+    out, err = p.communicate()
+    if b'XVID' in err:
+        print("TO PROCESS [%s]", filename)
+        p = subprocess.Pipe( ['ffmpeg', '-i', filename, '-c', 'copy', '-bsf:v', 'mpeg4_unpack_bframes', '-vtag', 'FMP4', tempfile])
+        p.wait() 
+        os.rename(tempfile, filename)
+
 def doFolder(folder):
     files = pathlib.Path(folder).glob('*')
     for f in files:
@@ -22,9 +33,9 @@ def doFolder(folder):
         else :
             if (f.name.endswith(".avi")) :
                 filename = "%s"%(os.path.join(folder,f.name)) 
-                print("processing %s"%filename)
-                os.system( "ffmpeg -i %s -c copy -bsf:v mpeg4_unpack_bframes -vtag FMP4 %s"%(filename,tempfile))
-                os.system( "mv %s %s"%(tempfile,filename))
+                doFile(filename)
+                #os.system( "ffmpeg -i %s -c copy -bsf:v mpeg4_unpack_bframes -vtag FMP4 %s"%(filename,tempfile))
+                #os.system( "mv %s %s"%(tempfile,filename))
 
 doFolder(directory)
 
